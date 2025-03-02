19cc033c54d8e7f5e3498e119411b0df8e9a7dc5
Orh <orhan.mamishov@gmail.com>
2025-03-03 01:50:51 +0300

    fix url

--- src/shared/utils/api/getDeliveriesRequest.ts
+++ src/shared/utils/api/getDeliveriesRequest.ts
@@ -1,16 +1,13 @@
 import axios from "axios";
+import { API_URL } from "../variables/api-url";
 
 export const getDeliveriesRequest = async (page: string, status?: string) => {
   if (status) {
     return await axios
-      .get(
-        `http://localhost:3000/api/deliveries/?status=${status}&page=${
-          page || 1
-        }`
-      )
+      .get(`${API_URL}/api/deliveries/?status=${status}&page=${page || 1}`)
       .then((res) => res.data.deliveries);
   }
   return await axios
-    .get(`http://localhost:3000/api/deliveries/?page=${page || 1}`)
+    .get(`${API_URL}/api/deliveries/?page=${page || 1}`)
     .then((res) => res.data.deliveries);
 };
--- src/shared/utils/api/getDeliveryRequest.ts
+++ src/shared/utils/api/getDeliveryRequest.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { API_URL } from "../variables/api-url";
 
 export const getDeliveryRequest = async (id: string) => {
   return await axios
-    .get(`http://localhost:3000/api/deliveries/?id=${id}`)
+    .get(`${API_URL}/api/deliveries/?id=${id}`)
     .then((res) => res.data);
 };
